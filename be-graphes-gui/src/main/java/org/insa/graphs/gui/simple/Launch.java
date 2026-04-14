1f540109baddddf056139b53632d3a25e08205b5
quetzalcoat1 <nathan.charlier05@gmail.com>
2026-04-14 17:41:08 +0200

    Launch : implémentation du main, sert à charger et afficher un graphe et un path

--- be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
+++ be-graphes-gui/src/main/java/org/insa/graphs/gui/simple/Launch.java
@@ -14,6 +14,7 @@ import org.insa.graphs.gui.drawing.components.BasicDrawing;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Path;
 import org.insa.graphs.model.io.BinaryGraphReader;
+import org.insa.graphs.model.io.BinaryPathReader;
 import org.insa.graphs.model.io.GraphReader;
 import org.insa.graphs.model.io.PathReader;
 
@@ -57,23 +58,27 @@ public class Launch {
         try (final GraphReader reader = new BinaryGraphReader(new DataInputStream(
                 new BufferedInputStream(new FileInputStream(mapName))))) {
 
-            // TODO: read the graph
-            graph = null;
+
+            graph = reader.read();
         }
 
         // create the drawing
         final Drawing drawing = createDrawing();
 
-        // TODO: draw the graph on the drawing
+        // draw the graph on the drawing
+        drawing.drawGraph(graph);
 
-        // TODO: create a path reader
-        try (final PathReader pathReader = null) {
 
-            // TODO: read the path
-            path = null;
+        // create a path reader
+        try (final PathReader pathReader = new BinaryPathReader(new DataInputStream(
+                new BufferedInputStream(new FileInputStream(pathName))))) {
+
+            // read the path
+            path = pathReader.readPath(graph);
         }
 
-        // TODO: draw the path on the drawing
+        // draw the path on the drawing
+        drawing.drawPath(path);
     }
 
 }

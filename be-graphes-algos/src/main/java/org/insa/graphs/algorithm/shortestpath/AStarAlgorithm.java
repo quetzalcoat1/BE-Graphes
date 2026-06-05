e558fa263f486dcba2230acc32d28786ce62b884
quetzalcoat1 <nathan.charlier05@gmail.com>
2026-06-05 14:32:42 +0200

    AStarAlgorithm : correction of cost calculation for TIME Mode search. Now based on estimated time with (distance / MaximumSpeed of graph)

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -1,5 +1,6 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.algorithm.AbstractInputData.Mode;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Point;
 
@@ -10,7 +11,13 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
     }
 
     @Override
-    public LabelStar initialisation(int i, Graph graph, ShortestPathData data) {
-        return new LabelStar(i, Point.distance(graph.get(i).getPoint(), data.getDestination().getPoint()));
+    public LabelStar initialisationLabel(int i, Graph graph, ShortestPathData data) {
+
+        if (data.getMode() == Mode.LENGTH) {
+            return new LabelStar(i, Point.distance(graph.get(i).getPoint(), data.getDestination().getPoint()));
+        }
+        else {  // This is the case when (data.getMode() == Mode.TIME), if another Mode is created, we need to change here
+            return new LabelStar(i, (double) (Point.distance(graph.get(i).getPoint(), data.getDestination().getPoint()) / (double) (data.getGraph().getGraphInformation().getMaximumSpeed()*1000))*3600);
+        }
     }
 }
